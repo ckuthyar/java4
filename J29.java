c7eb706489c4fcc1155ed27cc1de5fad67b8039e
ckuthyar <ckuthyar@gmail.com>
2025-10-15 05:22:28 +0530

    Gold Medal highest in English is 88

---
+++ J29.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+public class J29{
+   public static void main(String[] args) throws FileNotFoundException{
+   	String s1="";
+   	ArrayList<String> names = new ArrayList<>();
+   	ArrayList<Integer> sub1 = new ArrayList<>();
+   	File f1=new File("marks.txt");
+   	Scanner sc1=new Scanner(f1);
+   	while(sc1.hasNext()){
+   		s1=sc1.nextLine();
+   		String[] arr1=s1.split(",");
+   		names.add(arr1[0]);
+   		String[] arr2=arr1[3].split(":");
+   		sub1.add(Integer.parseInt(arr2[1]));
+
+   	}
+   	int maxSub1=0;
+   	int len1=sub1.size();
+   	for(int i=0;i<len1;i++){
+   		if (sub1.get(i)>maxSub1){
+   			maxSub1=sub1.get(i);
+   		}
+   	}
+   	System.out.println(maxSub1);
+   	System.out.println(names);
+   	System.out.println(sub1);
+	
+   }
+}
+
